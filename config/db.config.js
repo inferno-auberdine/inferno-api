b76f368e1fd4f4f1b27c6b55c6a851d8a1d6996f
Matthieu Audemard <matthieu.audemard@gmail.com>
2021-07-05 14:47:29 +0200

    fix: modifie la valeur de l'url de la bd

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@ const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || '27017';
 const DB_NAME = process.env.DB_NAME || 'inferno-app_db';
 const DB_USER = process.env.DB_USER;
-const DB_PASS = process.env.PASS;
+const DB_PASS = process.env.DB_PASS;
 
 let auth = '';
 
