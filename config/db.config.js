ea15689f7717a0979a34beb5a42d44d592e981b2
matthieuaudemard <matthieu.audemard@gmail.com>
2021-07-05 20:58:21 +0200

    ci: intègre la configuration de déploiement sur heroku (#2)

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,15 @@
+const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || '27017';
-const DB_URI = process.env.DB_URI || 'localhost';
-const DB_NAME = process.env.DB_NAME || 'inferno-app';
+const DB_NAME = process.env.DB_NAME || 'inferno-app_db';
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+
+let auth = '';
+
+if (DB_USER) {
+    auth = `${DB_USER}:${DB_PASS}@`
+}
 
 module.exports = {
-    url: `mongodb://${DB_URI}:${DB_PORT}/${DB_NAME}`
+    url: `mongodb://${auth}${DB_HOST}:${DB_PORT}/${DB_NAME}`
 };
--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const routes = require('./api/routes/raid.routes'); // import the routes
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const API_PORT = process.env.API_PORT || '3333';
+const API_PORT = process.env.PORT || 5000;
 
-const db = require("./api/models");
+const db = require('./api/models');
 
 const corsOptions = {origin: '*'};
 app.use(bodyParser.json());
@@ -13,20 +13,27 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(cors(corsOptions));
 
+app.use('/api', routes); //to use the routes
+
+app.route('/')
+  .get((req, res) => {
+    res.sendFile(process.cwd() + '/index.html');
+  });
+
+console.log(db.url);
+
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
   .then(() => {
-    console.log("Connected to the database!");
+    console.log(`Connected to the database!`);
   })
   .catch(err => {
-    console.log("Cannot connect to the database!", err);
+    console.log('Cannot connect to the database with connexion string ${db.url}!', err);
     process.exit();
   });
-
-app.use('/api', routes); //to use the routes
 
 const listener = app.listen(API_PORT, () => {
   console.log(`Your app is listening on port ${listener.address().port}`)
