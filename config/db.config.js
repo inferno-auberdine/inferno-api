0ee0a1a601a586e2a4e58dbfdcf19f1058d345b1
Matthieu Audemard <matthieu.audemard@gmail.com>
2021-07-05 13:20:41 +0200

    fix: modifie la valeur de l'url de la bd

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,9 @@
+const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || '27017';
-const DB_URI = process.env.DB_URI || 'localhost';
-const DB_NAME = process.env.DB_NAME || 'inferno-app';
+const DB_NAME = process.env.DB_NAME || 'inferno-app_db';
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.PASS;
 
 module.exports = {
-    url: `mongodb://${DB_URI}:${DB_PORT}/${DB_NAME}`
+    url: `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
 };
