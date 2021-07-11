8a6706f92d1ffcd5beb7f25aca67a16281a0db20
Matthieu Audemard <matthieu.audemard@gmail.com>
2021-07-11 03:20:12 +0200

    feat(raid): ajoute la route et la méthode du controller pour l'update

--- api/routes/raid.routes.js
+++ api/routes/raid.routes.js
@@ -3,5 +3,6 @@ const router  = express.Router();
 const raidController = require('../controllers/raid.controller');
 
 router.get('/raids', raidController.getAll);
+router.put('/raids/:id', raidController.update);
 
 module.exports = router; // export to use in server.js
