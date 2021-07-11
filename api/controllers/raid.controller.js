dc0d10c8283bc5d182ce2a89d7752e3cdf5ea69f
matthieuaudemard <matthieu.audemard@gmail.com>
2021-07-11 03:21:14 +0200

    feat(raid): ajoute la route et la méthode du controller pour l'update (#3)

--- api/controllers/raid.controller.js
+++ api/controllers/raid.controller.js
@@ -12,4 +12,31 @@ const getAll = (req, res) => {
     })
 };
 
-module.exports = {getAll};
+const update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: 'Data to update cannot be empty!'
+    });
+  }
+
+  const id = req.params.id;
+
+  Raid
+    .findByIdAndUpdate(id, req.body, {useFindAndModify: false, new: true})
+    .then(raidUpdate => {
+      if (!raidUpdate) {
+        res.status(404).json({
+          message: `Cannot update raid wit id=${id}.`
+        });
+      } else {
+        res.json(raidUpdate);
+      }
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: `Error occurred while updating raid with id ${id}`
+      });
+    });
+};
+
+module.exports = {getAll, update};
