3f852dafaa76b1dae5f5329b5b7c5ba434566083
Matthieu Audemard <matthieu.audemard@gmail.com>
2022-06-23 20:23:23 +0200

    fix: modifie le fichier de configuration de la DB
     * mise en conformité avec les données de clever-cloud qui est passé en replica set

--- config/db.config.js
+++ config/db.config.js
@@ -1,15 +1,5 @@
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_PORT = process.env.DB_PORT || '27017';
-const DB_NAME = process.env.DB_NAME || 'inferno-app_db';
-const DB_USER = process.env.DB_USER;
-const DB_PASS = process.env.DB_PASS;
-
-let auth = '';
-
-if (DB_USER) {
-    auth = `${DB_USER}:${DB_PASS}@`
-}
+const DB_CONNECTION_STR = process.env.DB_CONNECTION_STR;
 
 module.exports = {
-    url: `mongodb://${auth}${DB_HOST}:${DB_PORT}/${DB_NAME}`
+    url: DB_CONNECTION_STR
 };
